53bf00e7a87cbfbbec5b9f0fca433e448da70201
ViniciusHansen <vinicius.carlo.hansen@gmail.com>
2022-10-14 18:35:32 -0300

    add prototipo F

---
+++ subregional_br_22/F.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+set<char> setpy(string s){
+    set<char> st;
+    for(int i=0;i<s.size();i++)
+        st.insert(s[i]);
+    return st;
+}
+//desconsiderar o *
+int main(){
+    int n,c;
+    vector<string> words;
+    cin >> n >> c;
+    for(int i =0;i<n;i++){
+        string temp;
+        cin >> temp;
+        words.push_back();
+    }
+    map<char,int> o;
+    for(String word : words){
+        o.insert()
+    }
+        ocorrencias[setpy(word)]++;
+    //pegar a chave com maior ocorrencia
+
+
+
+
+
+
+
+    return 0;
+}
