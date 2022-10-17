a277598dfa30685e913cee60c09aad9f0f62e790
Vinícius Hansen <vinicius.carlo.hansen@gmail.com>
2022-10-17 19:47:01 -0300

    resolve a F com ajuda do pessoal do BRUTE

--- subregional_br_22/F.cpp
+++ subregional_br_22/F.cpp
@@ -2,55 +2,35 @@
 
 using namespace std;
 
-set<char> setpy(string s){
-    set<char> st;
-    for(int i=0;i<s.size();i++)
-        st.insert(s[i]);
-    return st;
-}
-
-bool compativel(string s, set<char> st){
-    int count=0;
-    for(int i=0;i<s.size();i++)
-        if(st.find(s[i]) != st.end()){    //elemento presente
-            count++;
-        }
-    return (count == s.size());
-}
 
 int main(){
-    int c=0,n,i,j;
+    int c,n,i,j,count=0,pos;
     cin >> n >> c;
     string s;
-    map<set<char>,int> ms;
-    vector<string> vs;
+    map<string,int> ms;
 
     for(i=0;i<n;i++){
         cin >> s;
-        vs.push_back(s);
-        if(ms[setpy(s)])
-            ms[setpy(s)]++;
-        else
-            ms[setpy(s)]=1;
+        for(j=0;j<s.size();j++){
+            if(s[j] == '*'){
+                pos = j;
+            }
+        }
+        for(char c='a';c<='z';c++){
+            s[pos]=c;
+            ms[s]++;
+        }
     }
 
-    //acha a maior key
     //ou seja, o set mais comum
     int maior = INT_MIN;
-    set<char> max_key;
+    string max_key;
     for(auto [k,v] : ms){
         if(v>maior){
             maior =v;
             max_key = k;
         }
     }
-
-    for(i=0;i<n;i++){
-        if(compativel(vs[i],max_key)){
-            c++;
-        }
-    }
-
-    cout << c <<endl;
+    cout << max_key << " " << ms[max_key] <<endl;
     return 0;
 }
